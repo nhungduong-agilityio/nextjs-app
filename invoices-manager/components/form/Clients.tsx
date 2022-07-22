f420d4d61fe89d352a4350a8eb1d340cbfd1746c
Nhung Duong <nhung.duong@asnet.com.vn>
2022-07-22 18:01:56 +0700

    Cache variables

--- invoices-manager/components/form/Clients.tsx
+++ invoices-manager/components/form/Clients.tsx
@@ -4,10 +4,11 @@ import { getClients } from 'apis'
 import { useQuery } from '@tanstack/react-query'
 
 const Clients: React.FC<{ width?: string | number }> = ({ width }) => {
-  const { data: clients } = useQuery<Array<Client>>(['clients'], getClients)
+  const { data } = useQuery<Array<Client>>(['clients'], getClients)
+  const clients = data || []
   return (
     <Select w={width}>
-      {clients?.map((item) => (
+      {clients.map((item) => (
         <option key={item.name} value={item.name}>
           {item.name}
         </option>
